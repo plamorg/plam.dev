026eb242a2f6439ff4a8ca878dd400f77c2e406a
smjleo <seojun.lee@oriel.ox.ac.uk>
2023-03-06 21:16:42 +0000

    feat: add transitions

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,10 +2,26 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'appear': 'appear 0.5s ease-in-out',
+				'fade-in': 'fade-in 0.4s ease-in-out',
+			}, 
+			keyframes: {
+				'appear': {
+					'0%': { opacity: 0, transform: 'translateY(10px)' },
+					'100%': { opacity: 1, transform: 'translateY(0)' },
+				}, 
+				'fade-in': {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			}
+		},
 		fontFamily: {
 			'sans': ['Epilogue', 'system-ui', 'sans-serif'],
-			'serif': ['Lora', 'ui-serif', 'Georgia'],		}
+			'serif': ['Lora', 'ui-serif', 'Georgia'],		
+		}
 	},
 	plugins: [],
 }
