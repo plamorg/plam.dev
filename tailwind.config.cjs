80fc8165512dad1c6e10bd7a6a23fb9af9a04403
smjleo <seojun.lee@oriel.ox.ac.uk>
2023-03-06 18:21:08 +0000

    feat: change font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			'sans': ['Epilogue', 'system-ui', 'sans-serif'],
+			'serif': ['Lora', 'ui-serif', 'Georgia'],		}
 	},
 	plugins: [],
 }
