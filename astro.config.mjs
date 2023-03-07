bc9e16fd1d3ebf69c34baff1d14d78a75e56dec7
smjleo <seojun.lee@oriel.ox.ac.uk>
2023-03-07 18:19:55 +0000

    ci: custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://plamorg.github.io',
+    site: 'https://plam.dev',
 });
